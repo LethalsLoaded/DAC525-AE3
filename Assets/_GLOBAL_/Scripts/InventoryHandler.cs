50d489abb6ccbfe496cbb99ccf789bb9c415c50b
Dan915 <33253673+Dan915@users.noreply.github.com>
2018-11-20 12:38:10 +0000

    Added header to InventoryHandler

--- Assets/_GLOBAL_/Scripts/InventoryHandler.cs
+++ Assets/_GLOBAL_/Scripts/InventoryHandler.cs
@@ -1,4 +1,13 @@
-﻿using UnityEngine;
+﻿/*
+ * This file has been created by Daniel Wesolowski for DAC525-AE3
+ * on Tue Nov 20 2018. Everything in this file is written
+ * by Daniel Wesolowski unless specified otherwise in a comment
+ * or if the file has been edited by another developer which
+ * is to be named below with the content added or modified mentioned
+ *
+ * NAME, DATE OF EDIT, CONTENT EDITED:
+ */
+using UnityEngine;
 
 public class InventoryHandler : MonoBehaviour
 {
