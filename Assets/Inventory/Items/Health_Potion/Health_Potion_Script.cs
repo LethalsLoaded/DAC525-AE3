541a223168bcd50b85fc44c610c5baa7aa2b2672
Dan915 <33253673+Dan915@users.noreply.github.com>
2018-11-20 21:05:13 +0000

    Implementation of Items
    +Item template
    +Health Potion Item

---
+++ Assets/Inventory/Items/Health_Potion/Health_Potion_Script.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * This file has been created by Daniel Wesolowski for DAC525-AE3
+ * on Tue Nov 20 2018. Everything in this file is written
+ * by Daniel Wesolowski unless specified otherwise in a comment
+ * or if the file has been edited by another developer which
+ * is to be named below with the content added or modified mentioned
+ *
+ * NAME, DATE OF EDIT, CONTENT EDITED:
+ */
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Health_Potion_Item", menuName = "Items/Potions/Health_Potion", order = 1)]
+public class Health_Potion_Script : Item 
+{
+	 public override void Use()
+     {
+         
+     }
+
+}
---
+++ Assets/Inventory/Items/Item.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * This file has been created by Daniel Wesolowski for DAC525-AE3
+ * on Tue Nov 20 2018. Everything in this file is written
+ * by Daniel Wesolowski unless specified otherwise in a comment
+ * or if the file has been edited by another developer which
+ * is to be named below with the content added or modified mentioned
+ *
+ * NAME, DATE OF EDIT, CONTENT EDITED:
+ */
+
+using UnityEngine;
+
+public enum ItemCategory
+{
+	USABLE,
+	WEAPON,
+	JUNK
+}
+public abstract class Item : ScriptableObject 
+{
+	[Header("- Item Information -")]
+	public string itemName;
+	public string itemDescription;
+	public ItemCategory itemCategory;
+	[Header("- Item Values -")]
+	public int itemValue;
+	public int itemPrice;
+	public int itemWeight;
+	[Header("- Item Objects -")]
+	public Sprite itemSprite;
+	public GameObject itemPrefab;
+	
+
+
+	public abstract void Use();
+	
+}
