eed012dbda013bdacfdd62d3950961a7e56f69d8
Dan915 <33253673+Dan915@users.noreply.github.com>
2018-12-25 15:35:32 +0000

    Room 23: NPC With Dialogue

---
+++ Assets/Levels/Levels_21_-_30/Level_23/Scripts/NPC_Dialogue_Script.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC_Dialogue_Script : MonoBehaviour {
+
+	public Text displayText;
+	public GameObject dialogue;
+	public string[] sentences;
+	int index;
+	public float speechSpeed;
+	public GameObject continueButton, skipButton;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine (Text());
+	}
+	void Update ()
+	{
+		if (displayText.text == sentences[index])
+		{
+			skipButton.SetActive(false);
+			continueButton.SetActive(true);
+		}
+		else
+		{
+			skipButton.SetActive(true);
+			continueButton.SetActive(false);
+		}
+	}
+	IEnumerator Text ()
+	{
+		foreach (char characters in sentences[index].ToCharArray())
+		{
+			displayText.text += characters;
+			yield return new WaitForSeconds(speechSpeed);
+		}
+	}
+	public void ContinueButton()
+	{
+		displayText.text = null;
+		speechSpeed = 0.1f;
+		if (index < sentences.Length -1)
+		{
+			index++;
+			continueButton.SetActive(false);
+			StartCoroutine (Text());
+		}
+		else
+		{
+			dialogue.SetActive(false);
+		}
+	}
+	public void SkipButton ()
+	{
+		speechSpeed = 0;
+		StopAllCoroutines();
+		displayText.text = null;
+		displayText.text = sentences[index];
+		skipButton.SetActive(false);
+		continueButton.SetActive(true);
+	}
+}
