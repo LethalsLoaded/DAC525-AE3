4bc9f8ed2d1d50b62e2bc155dc36b379105d3cda
Dan915 <33253673+Dan915@users.noreply.github.com>
2019-01-11 13:11:46 +0000

    Added Quit button and player staring position in multiple levels
    + quit button
    ~ starting position for all levels

---
+++ Assets/Levels/Levels_31_-_40/Level_36/Level_36_Ice_Script.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_36_Ice_Script : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.isTrigger && col.tag == "PLAYER")
+        GameManager.GetInstance().playerEntity.Hit(100, null);
+    }
+}
--- Assets/Levels/_Misc_Levels/Main_Menu/Scripts/MenuManager.cs
+++ Assets/Levels/_Misc_Levels/Main_Menu/Scripts/MenuManager.cs
@@ -2,19 +2,15 @@
 
 public class MenuManager : MonoBehaviour {
 
-	void Start ()
-	{
-		
-	}
 	
-	void Update ()
-	{
-		
-	}
 
 	public void StartGame()
 	{
 		var nextLevel = GameManager.GetInstance().playableLevels[GameManager.GetInstance().levelIndex];
         UnityEngine.SceneManagement.SceneManager.LoadScene(nextLevel.GetSceneName());
+	}
+	public void QuitGame()
+	{
+		Application.Quit();
 	}
 }
